c373b07818943dc42b3826d7b1bbf7d6ecb41fb1
rjz <rj@rjzaworski.com>
2017-01-08 12:20:00 -0800

    In the beginning

---
+++ README.md
@@ -0,0 +1,104 @@
+Microask
+===============================================================================
+
+µ-scale feedback form
+
+![](microask.gif)
+
+Usage
+-------------------------------------------------------------------------------
+
+Install via `npm`:
+
+```ShellSession
+$ npm i --save microask
+```
+
+Next, add the stylesheet and a container element to your page's markup:
+
+```html
+<!-- in <head /> -->
+<link rel="stylesheet" href="./node_modules/microask/microask.css" />
+
+<!-- in <body /> -->
+<div id="my-ask"></div>
+```
+
+Finally, require `microask` and instantiate it in an existing DOM element:
+
+```js
+const Microask = require('microask');
+
+window.addEventListener('DOMContentLoaded', function () {
+  const el = document.getElementById('my-ask');
+  const ask = new Microask(el);
+
+  ask
+    .update('<p>Hello, world!</p>')
+    .show();
+});
+```
+
+API
+-------------------------------------------------------------------------------
+
+All methods return an instance of `Microask` to enable chaining.
+
+### `.show(0) => Microask`
+
+Show the microask.
+
+### `.hide(0) => Microask`
+
+Hide the microask.
+
+### `.hideAfter(delay: Number) => Microask`
+
+Delay hide until `delay` ms have elapsed
+
+### `.update(htmlStr: String, callback: Function?) => Microask`
+
+Set microask HTML to `htmlStr`. If `callback` is provided, it will receive the
+updated element once the update is complete.
+
+### `.updateAfter(htmlStr: String, delay: Number, callback: Function?) => Microask`
+
+Delay update until `delay` ms have elapsed
+
+### `.buttonSchene(opts: Object) => Microask`
+
+Render a button scene.
+
+```js
+ask.buttonScene({
+  id: 'buttons',
+  prompt: 'Can you click it?',
+  actions: [
+    {
+      label: 'Click me',
+      value: 'ok',
+      callback: ask => ask
+        .updateAfter('<p>Good job!</p>', 100)
+        .hideAfter(2000),
+    },
+  ],
+});
+```
+
+### `.textSchene(opts: Object) => Microask`
+
+Render a text scene.
+
+```js
+ask.textScene({
+  id: 'email',
+  prompt: 'What\'s on your mind?',
+  placeholder: 'Start typing',
+  callback: ask => ask.hide(),
+});
+```
+
+License
+-------------------------------------------------------------------------------
+
+MIT
---
+++ microask.js
@@ -0,0 +1,143 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+module.exports = Widget;
+
+function Widget (el) {
+  this.el = el;
+  el.className += ' microask'
+  this._className = el.className;
+  this._isHiding = null;
+
+  el.innerHTML = '';
+  this._appendContent('');
+}
+
+util.inherits(Widget, EventEmitter);
+
+var contentClasses = {
+  base: 'microask__content',
+  enter: 'microask__content--entering',
+  exit: 'microask__content--exiting',
+};
+
+var TRANSITION_TIME = 500;
+var keys = {
+  ENTER: 13,
+};
+
+Widget.prototype._appendContent = function (htmlStr) {
+  var content = document.createElement('div');
+  content.className = [
+    contentClasses.base,
+    contentClasses.enter,
+  ].join(' ');
+  content.innerHTML = htmlStr;
+  this.el.appendChild(content);
+
+  // Trigger animation immediately
+  setTimeout(function () {
+    content.className = contentClasses.base;
+  }, 10);
+  this.content = content;
+  console.log(this.content.clientHeight);
+  this.el.style.height = this.content.clientHeight + 'px';
+  return content;
+}
+
+Widget.prototype._emitEvent = function (id, action, value) {
+  this.emit('event', {
+    id: id,
+    action: action,
+    value: value,
+  });
+};
+
+Widget.prototype.update = function (htmlStr, cb) {
+  var oldContent = this.content;
+  oldContent.className = [
+    contentClasses.base,
+    contentClasses.exit,
+  ].join(' ');
+
+  setTimeout(function () {
+    oldContent.parentNode.removeChild(oldContent);
+  }, TRANSITION_TIME);
+
+  var newContent = this._appendContent(htmlStr);
+  if (cb && typeof cb === 'function') {
+    cb(newContent);
+  }
+  return this;
+};
+
+Widget.prototype.buttonScene = function (opts) {
+  var widget = this;
+  var actions = opts.actions;
+  var onButtonClick = function (button, i) {
+    button.addEventListener('click', function (e) {
+      var value = actions[i].value || actions[i].label;
+      e.preventDefault();
+      widget._emitEvent(opts.id, 'click', value);
+      actions[i].callback.call(null, widget, e);
+    });
+  };
+
+  widget.update([
+    '<label>' + opts.prompt + '</label>',
+    actions.map(function (action) {
+      return '<button>' + action.label + '</button>';
+    }).join(''),
+  ].join(''), function (el) {
+    [].map.call(el.getElementsByTagName('button'), onButtonClick);
+  });
+  return this;
+};
+
+Widget.prototype.textScene = function (opts) {
+  var widget = this;
+  widget.update([
+    '<label>' + opts.prompt + '</label>',
+    '<input type="text" placeholder="' + opts.placeholder + '" />',
+  ].join(''), function (el) {
+    [].map.call(el.getElementsByTagName('input'), function (input, i) {
+      input.addEventListener('keypress', function (e) {
+        if (e.keyCode === keys.ENTER) {
+          e.preventDefault();
+          widget._emitEvent(opts.id, 'submit', e.target.value);
+          opts.callback && opts.callback.call(null, widget);
+        }
+      });
+    });
+  });
+  return this;
+};
+
+Widget.prototype.updateAfter = function (htmlStr, delay, cb) {
+  window.setTimeout(this.update.bind(this, htmlStr, cb), delay);
+  return this;
+};
+
+Widget.prototype.show = function () {
+  clearTimeout(this._isHiding);
+  this.el.className = this._className + ' active';
+  return this;
+};
+
+Widget.prototype.hide = function (delay) {
+  clearTimeout(this._isHiding);
+  this.el.className = this._className;
+  return this;
+};
+
+Widget.prototype.hideAfter = function (delay) {
+  var finalDelay = delay || 0;
+  var widget = this;
+  if (!this._isHiding) {
+    this._isHiding = window.setTimeout(function () {
+      widget.hide();
+      widget._isHiding = null;
+    }, delay);
+  }
+  return this;
+};
